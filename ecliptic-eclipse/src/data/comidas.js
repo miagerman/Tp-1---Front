70534e6f6fe5e1c4500e085bcf104b110458be2e
miagerman <49194476@est.ort.edu.ar>
2026-04-10 10:50:22 -0300

    Lo dividi todo bien y mejore el readme y lo hice mas lindo

---
+++ ecliptic-eclipse/src/data/comidas.js
@@ -0,0 +1,10 @@
+export const comidas = [
+    { nombre: "Pizza", categoria: "Rapida", emoji: "🍕" },
+    { nombre: "Hamburguesa", categoria: "Rapida", emoji: "🍔" },
+    { nombre: "Empanadas", categoria: "Tradicional", emoji: "🥟" },
+    { nombre: "Asado", categoria: "Tradicional", emoji: "🥩" },
+    { nombre: "Ensalada", categoria: "Saludable", emoji: "🥗" },
+    { nombre: "Sushi", categoria: "Saludable", emoji: "🍣" },
+    { nombre: "Helado", categoria: "Postre", emoji: "🍨" },
+    { nombre: "Torta", categoria: "Postre", emoji: "🍰" }
+  ];
